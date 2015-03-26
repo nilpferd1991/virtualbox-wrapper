008f85df911e0c8b85289ef07974591be9a6f86e
Nils <area51.nils@googlemail.com>
2015-03-26 23:12:27 +0100

    Added contextmanager for sessions and storage controller

--- virtualbox/first_test/main.py
+++ virtualbox/first_test/main.py
@@ -1,11 +1,17 @@
 from vboxapi import VirtualBoxManager
+from contextlib import contextmanager
 import time
 
 # Some general remarks:
 # There is the VirtualBoxManager with the vbox and there is a machine object
 # for every box there is on the computer.
 # A machine can be started. For this we need the current session object.
-#
+# We also need this to lock the machine and change its parameters.
+# Session objects represent a client process (and are a process by themselves whereas the machines life
+# in the main process)
+
+# The important part is the IVirtualBox. To do anything useful, we need this object.
+# It is implemented as a singleton.
 
 class MachineHandler:
     settings_folder = "/media/Daten/Projects/BlueYonder/virtualbox/first_test/test/"
@@ -13,10 +19,25 @@ class MachineHandler:
     def __init__(self):
         self.virtual_box_manager = VirtualBoxManager()
         self.virtual_box = self.virtual_box_manager.vbox
-
+        self.constants = self.virtual_box_manager.constants
         self.machines = dict()
-
         self.get_machines()
+
+    @contextmanager
+    def get_session(self, machine=None):
+        session = self.virtual_box_manager.getSessionObject(self.virtual_box)
+        if machine is not None:
+            machine.lockMachine(session, self.constants.LockType_Shared)
+            if session.state != self.constants.SessionState_Locked:
+                raise Exception
+
+        try:
+            yield session
+        finally:
+            if session.state == self.constants.SessionState_Locked:
+                session.unlockMachine()
+            else:
+                print "not locked!"
 
     def get_machines(self):
         for machine in self.virtual_box_manager.getArray(self.virtual_box, "machines"):
@@ -40,19 +61,21 @@ class MachineHandler:
             raise KeyError
 
         machine = self.machines[machine_name]
-        session = self.virtual_box_manager.getSessionObject(self.virtual_box)
-        progress = machine.launchVMProcess(session, "headless", "")
-        progress.waitForCompletion(-1)
-        self.virtual_box_manager.closeMachineSession(session)
+
+        with self.get_session() as session:
+            progress = machine.launchVMProcess(session, "headless", "")
+            # Wait until startup has completed
+            progress.waitForCompletion(-1)
+            # when startup is completed, the running machine lives in its own session object
+            # so we can reuse this session. For this we have to give away the lock.
 
     def remove_machine(self, machine_name):
         if machine_name not in self.machines:
             raise KeyError
 
-        machine = self.virtual_box.findMachine(machine_name)
-        machine = machine.unregister(self.virtual_box_manager.constants.CleanupMode_Full)
-        if machine:
-            machine.deleteSettings()
+        machine = self.machines[machine_name]
+        media = machine.unregister(self.constants.CleanupMode_Full)
+        machine.deleteConfig(media)
 
         del self.machines[machine_name]
 
@@ -61,13 +84,13 @@ class MachineHandler:
             raise KeyError
 
         machine = self.machines[machine_name]
-        session = self.virtual_box_manager.getSessionObject(self.virtual_box)
-        machine.lockMachine(session, self.virtual_box_manager.constants.LockType_Shared)
-        mutable_machine = session.machine
-        for var in kwargs:
-            mutable_machine.__setattr__(var, kwargs[var])
 
-        mutable_machine.saveSettings()
+        with self.get_session(machine) as session:
+            mutable_machine = session.machine
+            for var in kwargs:
+                mutable_machine.__setattr__(var, kwargs[var])
+
+            mutable_machine.saveSettings()
 
     def get_machine_property(self, machine_name, property):
         if machine_name not in self.machines:
@@ -76,11 +99,34 @@ class MachineHandler:
         machine = self.machines[machine_name]
         return machine.__getattr__(property)
 
+    def stop_machine(self, machine_name):
+        if machine_name not in self.machines:
+            raise KeyError
+
+        machine = self.machines[machine_name]
+
+        with self.get_session(machine) as session:
+            console = session.console
+            progress = console.powerDown()
+            progress.waitForCompletion(-1)
+            time.sleep(0.001)
+
+    def call_machine_function(self, machine_name, function):
+        if machine_name not in self.machines:
+            raise KeyError
+
+        machine = self.machines[machine_name]
+
+        with self.get_session(machine) as session:
+            mutable_machine = session.machine
+            function(mutable_machine)
+            mutable_machine.saveSettings()
+
 
 if __name__ == "__main__":
     machine_handler = MachineHandler()
     #machine_handler.create_new_machine("test", "Linux")
-    #machine_handler.start_machine("test")
     #machine_handler.remove_machine("test")
 
-    test_machine = machine_handler.machines["test"]
+    machine_handler.call_machine_function("test", lambda machine:
+        machine.addStorageController("storage", machine_handler.constants.StorageBus_SATA))
