fd06be8db38b98d9700640be7e5b14bb73b92752
Nils <area51.nils@googlemail.com>
2015-03-28 00:16:00 +0100

    Added more storage controls

--- virtualbox/first_test/main.py
+++ virtualbox/first_test/main.py
@@ -1,4 +1,5 @@
 from vboxapi import VirtualBoxManager
+import xpcom
 from contextlib import contextmanager
 import time
 
@@ -56,14 +57,15 @@ class MachineHandler:
         self.virtual_box.registerMachine(new_machine)
         self.machines[machine_name] = new_machine
 
-    def start_machine(self, machine_name):
+    def start_machine(self, machine_name, kind):
+        # kind in [headless, gui]
         if machine_name not in self.machines:
             raise KeyError
 
         machine = self.machines[machine_name]
 
         with self.get_session() as session:
-            progress = machine.launchVMProcess(session, "headless", "")
+            progress = machine.launchVMProcess(session, kind, "")
             # Wait until startup has completed
             progress.waitForCompletion(-1)
             # when startup is completed, the running machine lives in its own session object
@@ -119,14 +121,63 @@ class MachineHandler:
 
         with self.get_session(machine) as session:
             mutable_machine = session.machine
-            function(mutable_machine)
+
+            try:
+                function(mutable_machine)
+            except xpcom.Exception as e:
+                print e.msg
             mutable_machine.saveSettings()
 
+    def add_storage(self, machine_name, storage_controller_name, kind):
+        def add_storage_impl(machine):
+            machine.addStorageController(storage_controller_name, kind)
+
+        self.call_machine_function(machine_name, add_storage_impl)
+
+    def attach_device(self, machine_name, hard_disk_name, storage_controller_name, port):
+        file_name = self.settings_folder + hard_disk_name + ".vdi"
+
+        new_medium = None
+
+        for media in self.virtual_box_manager.getArray(self.virtual_box, "hardDisks"):
+            if media.location == file_name:
+                new_medium = media
+                if self.virtual_box_manager.getArray(new_medium, "machineIds"):
+                    raise NotImplementedError
+                new_medium.deleteStorage()
+
+        if not new_medium:
+            new_medium = self.virtual_box.createHardDisk("vdi", file_name)
+
+        progress = new_medium.createBaseStorage(10 * 1024*1024*1024, (self.constants.MediumVariant_Standard, ))
+        progress.waitForCompletion(-1)
+
+        media = self.virtual_box.openMedium(file_name, self.constants.DeviceType_HardDisk,
+                                            self.constants.AccessMode_ReadWrite, True)
+
+        def attach_device_impl(machine):
+            machine.attachDevice(storage_controller_name, port, 0, self.constants.DeviceType_HardDisk, media)
+
+        self.call_machine_function(machine_name, attach_device_impl)
+
+    def detach_device(self, machine_name, storage_controller_name, port):
+        def detach_device_impl(machine):
+            machine.detachDevice(storage_controller_name, port, 0)
+
+        self.call_machine_function(machine_name, detach_device_impl)
+
+    def attach_dvd(self, machine_name, storage_controller_name, port, img_location):
+        media = self.virtual_box.openMedium(img_location, self.constants.DeviceType_DVD,
+                                            self.constants.AccessMode_ReadOnly, True)
+
+        def attach_device_impl(machine):
+            machine.attachDevice(storage_controller_name, port, 0, self.constants.DeviceType_DVD, media)
+
+        self.call_machine_function(machine_name, attach_device_impl)
 
 if __name__ == "__main__":
     machine_handler = MachineHandler()
-    #machine_handler.create_new_machine("test", "Linux")
-    #machine_handler.remove_machine("test")
+    machine_handler.detach_device("test", "storage", 1)
+    machine_handler.attach_dvd("test", "storage", 1, machine_handler.settings_folder + "debian-7.8.0-i386-CD-1.iso")
+    machine_handler.start_machine("test", "gui")
 
-    machine_handler.call_machine_function("test", lambda machine:
-        machine.addStorageController("storage", machine_handler.constants.StorageBus_SATA))
